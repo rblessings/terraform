33637cd0c7effad6d6abfdd0b7c3069d09189b00
Ranzy Blessings <blessingsihembi@gmail.com>
2024-12-18 15:29:01 +0200

    feat: validate user input and trim leading/trailing spaces

--- main.tf
+++ main.tf
@@ -19,22 +19,37 @@ terraform {
 variable "control_plane_node_host" {
   type        = string
   default     = "https://10.38.125.65:16443"
-  description = "URL of the control plane node, including protocol, IP address, and port."
+  description = "URL of the control plane node, including protocol, IP address, and optional port."
+
+  # Validation to ensure the control_plane_node_host matches the required URL format:
+  # - The URL must start with 'https://'.
+  # - It can be either:
+  #   - A valid IPv4 address (e.g., 192.168.1.1), or
+  #   - A valid domain name (e.g., example.com).
+  # - Optionally, it may include a port number (1-65535).
+  validation {
+    condition = can(
+      regex("^https:\\/\\/((?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}|(?:[0-9]{1,3}\\.){3}[0-9]{1,3})(?::[0-9]{1,5})?$",
+      var.control_plane_node_host)
+    )
+    error_message = "The control_plane_node_host must be in the format https://<IP_ADDRESS|DOMAIN_NAME>:<PORT>"
+  }
 }
 
 variable "kube_config_path" {
   type        = string
   default     = "~/.kube/microk8s-config"
-  description = ""
+  description = "Path to the Kubernetes config file used for authentication and cluster access."
+
   validation {
-    condition     = contains(["~/.kube/config", "~/.kube/microk8s-config"], var.kube_config_path)
-    error_message = "The valid kube config path should be either '~/.kube/config' or '~/.kube/microk8s-config'."
+    condition     = fileexists(var.kube_config_path)
+    error_message = "The file at kube_config_path does not exist. Please provide a valid Kubernetes config path."
   }
 }
 
 provider "kubernetes" {
-  host        = var.control_plane_node_host
-  config_path = var.kube_config_path
+  host        = trimspace(var.control_plane_node_host)
+  config_path = trimspace(var.kube_config_path)
 }
 
 resource "kubernetes_deployment" "test" {
